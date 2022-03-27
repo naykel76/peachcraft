433c1fe3bd0490390b97cf0ad555b9fafe56fda7
naykel76 <32632005+naykel76@users.noreply.github.com>
2022-03-28 05:02:35 +1000

    add sql rest

--- nk_reset.sql
+++ nk_reset.sql
@@ -8,3 +8,15 @@ TRUNCATE TABLE `oc_paypal_order_transaction`;
 TRUNCATE TABLE `oc_paypal_order`;
 TRUNCATE TABLE `oc_voucher_theme_description`;
 TRUNCATE TABLE `oc_voucher_theme`;
+
+
+--
+--
+--
+
+TRUNCATE TABLE `oc_voucher_theme_description`;
+INSERT INTO `oc_voucher_theme_description` (`voucher_theme_id`, `language_id`, `name`) VALUES ('1', '1', 'Peach Craft Voucher');
+
+--
+--
+--
